a1535b79284c64f3b6894c3aaffee26e1141363b
iyuroch <iyuroch@gmail.com>
2017-07-11 15:42:17 +0300

    History changes
    Changed hash URI to pushState.
    If you load article as new - it doesnt work properly.

--- app/index.js
+++ app/index.js
@@ -3,8 +3,7 @@ $(document).ready(function() {
 
   //parse current url
   {
-    var currentURL = window.location.pathname.substr(1);
-
+    var currentURL = window.location.href.split('/')[3];
     if (currentURL.length <= 1) {
       var currentURL = 'index';
     }
@@ -18,7 +17,7 @@ $(document).ready(function() {
     $(".spinner").removeClass("spinner-stop");
     $("li").removeClass("active");
     $('a[href="'+currentURL+'"]').closest("li").addClass("active");
-    $.get( "html/" + currentURL + ".html", function( data ) {
+    $.get( "/html/" + currentURL + ".html", function( data ) {
       $( ".content" ).html( data );
     });
     $(".content").removeClass("content-blurr");
@@ -26,11 +25,10 @@ $(document).ready(function() {
   }
 
   //response for a link in navigation clicked
-  $( "a" ).on("click", function(event){
+  $( "a.links" ).on("click", function(event){
     event.preventDefault();
-    var currentURL = $(this).attr("href").split(".")[0]
-    history.pushState("", "", currentURL);
+    var currentURL = $(this).attr("href");
+    history.pushState("", "", "/" + currentURL);
     loadState(currentURL);
-
  });
 });
--- app/load.js
+++ app/load.js
@@ -4,7 +4,6 @@
 {
 $(".content").addClass("content-blurr");
 $(".spinner").removeClass("spinner-stop");
-$('.content').after('<div id="myModal" class="modal fade" role="dialog"> <div class="modal-dialog modal-lg"> <div class="modal-content"> <div class="modal-header"> <button type="button" class="close" data-dismiss="modal">&times;</button> <h4 class="modal-title">Modal Header</h4> </div><div class="modal-body"> <p>Some text in the modal.</p></div><div class="modal-footer"> <button type="button" class="btn btn-default" data-dismiss="modal">Close</button> </div></div></div></div>');
 }
 
 var recData;
@@ -12,18 +11,20 @@ var recData;
 
 //response for click on for detailed info - creates and pop modal with info
 $('#myModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget)
-  var itemIndex = button.data('index')
-  var modal = $(this)
-  var changeName = "content"
-  modal.find('.modal-title').text(recData.items[itemIndex].title)
-  modal.find('.modal-body').html(recData.items[itemIndex].content)
-  location.hash = recData.items[itemIndex].title
+  var button = $(event.relatedTarget);
+  var itemIndex = button.data('index');
+  var modal = $(this);
+  var changeName = "content";
+  modal.find('.modal-title').text(recData.items[itemIndex].title);
+  modal.find('.modal-body').html(recData.items[itemIndex].content);
+  console.log(recData.items[itemIndex].title);
+  history.pushState("","", "/" + window.location.pathname.split("/")[1] + "/"+ recData.items[itemIndex].title);
+  //location.hash = recData.items[itemIndex].title
 });
 
 //changes hash when modal dismissed
 $('#myModal').on('hide.bs.modal', function (e) {
-  window.location.hash = ''
+  history.pushState("", "", "/" + window.location.pathname.split("/")[1]);
 });
 
 
@@ -34,12 +35,12 @@ $( "ul.list-group > a" ).click(function( event ) {
 
 //ajax blogger and DOM action after response
 $.ajax({
-  url: "https://www.googleapis.com/blogger/v3/blogs/679169126963230298/posts/search?q=label:"+window.location.pathname.substr(1)+"&key=AIzaSyB32MdE0PmSCK3tgVhVFkv0F1KloTzQSCY",
+  url: "https://www.googleapis.com/blogger/v3/blogs/679169126963230298/posts/search?q=label:" + window.location.href.split("/")[3] + "&key=AIzaSyB32MdE0PmSCK3tgVhVFkv0F1KloTzQSCY",
   }).done(function(data) {
     recData = data
     var array = ''
     $.each( recData.items, function( key, value ) {
-      array += ("<li class='list-group-item'><a href='"+location.pathname+'#'+value.title+"' class='btn btn-link' type='button' data-toggle='modal' data-target='#myModal' data-index='"+key+"'>"+value.title+"</></li>");
+      array += ("<li class='list-group-item'><a href='"+location.pathname+'/'+value.title+"' class='btn btn-link' type='button' data-toggle='modal' data-target='#myModal' data-index='"+key+"'>"+value.title+"</></li>");
     });
     $(".content").append("<ul class='list-group'>"+array+"</ul>");
 
@@ -51,7 +52,7 @@ $.ajax({
 
 //act accordingly to hash URL of the page - if some - pop modal
 function popModal(){
-  var splicedHash = decodeURI(location.hash.substr(1))
+  var splicedHash = decodeURI(window.location.pathname.split("/")[2]);
 
   if (splicedHash.length != 0) {
     $.each( recData.items, function( key, value ) {
